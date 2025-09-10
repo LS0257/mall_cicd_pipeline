4c3902218ac35c04495da7c5750bca63a136fd2a
Lavanya Sharma <radhika.sharma.june@gmail.com>
2025-09-10 09:30:16 +0530

    Commit for Deployment 1.2

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,12 +4,12 @@ from sklearn.ensemble import RandomForestClassifier
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))  # go one level up
-data_path= os.path.join(BASE_DIR, 'mall_customer_model.pkl')
+data_path= os.path.join(BASE_DIR, 'mall_customers_model.pkl')
 model = joblib.load(data_path)
 
 class TestModelTraining(unittest.TestCase):
     def test_model_training(self):
-        # model = joblib.load('mall_customer_model.pkl')
+        # model = joblib.load('mall_customers_model.pkl')
         self.assertIsInstance(model, RandomForestClassifier)
         self.assertGreaterEqual(len(model.feature_importances_), 4)
 if __name__ == '__main__':
